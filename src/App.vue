781a475ff2417558fe65c1f7e5a5933d754064df
Alan Nguyen <alan.t.ngn@gmail.com>
2018-10-13 19:03:12 +0200

    Add logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
     <v-app id="inspire">
+        <v-layout >
+            <v-flex
+                    xs2 offset-xs5
+                    md2 offset-md5
+                    lg2 offset-lg5>
+
+                <img :src="logoSrc" style="width:100%"/>
+            </v-flex>
+        </v-layout>
+
         <v-layout v-if="loading" >
           <v-flex xs12 text-xs-center>
             <v-progress-circular
@@ -19,12 +29,14 @@
 <script>
 import MembersList from './components/MembersList.vue'
 import axios from 'axios'
+import logo from './assets/logo.png'
 export default {
     name: 'app',
     data() {
       return {
         users: [],
-        loading: true
+        loading: true,
+        logoSrc: logo,
       }
     },
     components: {
